3bcdab38fbc582c6a8ca0e7072ae4b47cc19343b
Nikita Karpuk <bitxon.dev@gmail.com>
2023-02-06 00:00:31 +0100

    JS Implementation of sidecar pattern

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Hazelcast Architectural Patterns
 
-## Modules
-- [Hazelcast Embedded](hazelcast-embedded/)
-- [Hazelcast Sidecar](hazelcast-sidecar/)
-- [Hazelcast Client-Server](hazelcast-client-server/)
+## Examples
+
+- Hazelcast Embedded ( [Java](hazelcast-embedded/) )
+- Hazelcast Sidecar ( [Java](hazelcast-sidecar/) | [JS](hazelcast-sidecar-js/) )
+- Hazelcast Client-Server ( [Java](hazelcast-client-server/) )
 
 ## Useful Links
+
 - [Architectural Patterns Description](https://hazelcast.com/blog/architectural-patterns-for-caching-microservices/)
 - [Distributed Data Structures](https://docs.hazelcast.com/imdg/4.2/data-structures/distributed-data-structures)
---
+++ hazelcast-sidecar-js/index.js
@@ -0,0 +1,75 @@
+import express from "express";
+import { setTimeout } from "timers/promises";
+import { Client } from "hazelcast-client";
+
+const port = 8080;
+const app = express();
+const hzConfig = {
+  clusterName: "hzcluster",
+  network: {
+    clusterMembers: ["localhost:5701"],
+  },
+};
+
+(async () => {
+  try {
+    const hzClient = await Client.newHazelcastClient(hzConfig);
+    const hzCpSubsystem = await hzClient.getCPSubsystem();
+    const mainCache = await hzClient.getMap("main-cache");
+    const lockedCache = await hzClient.getMap("locked-cache");
+
+    app.get("/cache/:key", async (req, res) => {
+      const { key } = req.params;
+      const value = await mainCache.get(key);
+      res.send(value);
+    });
+
+    app.put("/cache/:key/:value", async (req, res) => {
+      const { key, value } = req.params;
+      await mainCache.put(key, value);
+      res.send(value);
+    });
+
+    app.get("/fenced-lock-cache/:key", async (req, res) => {
+      const { key } = req.params;
+
+      const lock = await hzCpSubsystem.getLock(key);
+      const fence = await lock.tryLock(6000);
+      if (fence !== undefined) {
+        try {
+          const value = await lockedCache.get(key);
+          res.send(value);
+        } finally {
+          await lock.unlock(fence);
+        }
+      } else {
+        res.status(408).send();
+      }
+    });
+
+    app.put("/fenced-lock-cache/:key/:value", async (req, res) => {
+      const { key, value } = req.params;
+
+      const lock = await hzCpSubsystem.getLock(key);
+      const fence = await lock.tryLock(6000);
+      if (fence !== undefined) {
+        try {
+          await lockedCache.remove(key);
+          await setTimeout(5000); // pretend that this is long operation
+          await lockedCache.put(key, value);
+          res.send(value);
+        } finally {
+          await lock.unlock(fence);
+        }
+      } else {
+        res.status(408).send();
+      }
+    });
+
+    app.listen(port, () => {
+      console.log(`your server is running on port ${port}`);
+    });
+  } catch (err) {
+    console.error("Error occurred:", err);
+  }
+})();
