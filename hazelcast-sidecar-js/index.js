0cf91de0986b984c49cb0c5e1e240b3f7a95ea5c
Nikita Karpuk <bitxon.dev@gmail.com>
2023-05-07 16:52:51 +0200

    Hazelcast Sidecar (JS): Upgrade libs and remove anonymous function

--- hazelcast-sidecar-js/index.js
+++ hazelcast-sidecar-js/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { setTimeout } from "timers/promises";
 import { Client } from "hazelcast-client";
 
+
 const port = 8080;
 const app = express();
 const hzConfig = {
@@ -9,67 +10,78 @@ const hzConfig = {
   network: {
     clusterMembers: ["localhost:5701"],
   },
+  connectionStrategy: {
+    connectionRetry: {
+      clusterConnectTimeoutMillis: 10000,
+    },
+  },
 };
 
-(async () => {
-  try {
-    const hzClient = await Client.newHazelcastClient(hzConfig);
-    const hzCpSubsystem = await hzClient.getCPSubsystem();
-    const mainCache = await hzClient.getMap("main-cache");
-    const lockedCache = await hzClient.getMap("locked-cache");
 
-    app.get("/cache/:key", async (req, res) => {
-      const { key } = req.params;
-      const value = await mainCache.get(key);
-      res.send(value);
-    });
+async function start() {
+  const hzClient = await Client.newHazelcastClient(hzConfig);
+  const hzCpSubsystem = await hzClient.getCPSubsystem();
+  const mainCache = await hzClient.getMap("main-cache");
+  const lockedCache = await hzClient.getMap("locked-cache");
 
-    app.put("/cache/:key/:value", async (req, res) => {
-      const { key, value } = req.params;
-      await mainCache.put(key, value);
-      res.send(value);
-    });
+  app.get("/cache/:key", async (req, res) => {
+    const { key } = req.params;
+    const value = await mainCache.get(key);
+    res.send(value);
+  });
 
-    app.get("/fenced-lock-cache/:key", async (req, res) => {
-      const { key } = req.params;
+  app.put("/cache/:key/:value", async (req, res) => {
+    const { key, value } = req.params;
+    await mainCache.put(key, value);
+    res.send(value);
+  });
 
-      const lock = await hzCpSubsystem.getLock(key);
-      const fence = await lock.tryLock(6000);
-      if (fence !== undefined) {
-        try {
-          const value = await lockedCache.get(key);
-          res.send(value);
-        } finally {
-          await lock.unlock(fence);
-        }
-      } else {
-        res.status(408).send();
+  app.get("/fenced-lock-cache/:key", async (req, res) => {
+    const { key } = req.params;
+
+    const lock = await hzCpSubsystem.getLock(key);
+    const fence = await lock.tryLock(6000);
+    if (fence !== undefined) {
+      try {
+        const value = await lockedCache.get(key);
+        res.send(value);
+      } finally {
+        await lock.unlock(fence);
       }
-    });
+    } else {
+      res.status(408).send();
+    }
+  });
 
-    app.put("/fenced-lock-cache/:key/:value", async (req, res) => {
-      const { key, value } = req.params;
+  app.put("/fenced-lock-cache/:key/:value", async (req, res) => {
+    const { key, value } = req.params;
 
-      const lock = await hzCpSubsystem.getLock(key);
-      const fence = await lock.tryLock(6000);
-      if (fence !== undefined) {
-        try {
-          await lockedCache.remove(key);
-          await setTimeout(5000); // pretend that this is long operation
-          await lockedCache.put(key, value);
-          res.send(value);
-        } finally {
-          await lock.unlock(fence);
-        }
-      } else {
-        res.status(408).send();
+    const lock = await hzCpSubsystem.getLock(key);
+    const fence = await lock.tryLock(6000);
+    if (fence !== undefined) {
+      try {
+        await lockedCache.remove(key);
+        await setTimeout(5000); // pretend that this is long operation
+        await lockedCache.put(key, value);
+        res.send(value);
+      } finally {
+        await lock.unlock(fence);
       }
-    });
+    } else {
+      res.status(408).send();
+    }
+  });
 
-    app.listen(port, () => {
-      console.log(`your server is running on port ${port}`);
-    });
-  } catch (err) {
-    console.error("Error occurred:", err);
-  }
-})();
+  app.listen(port, () => {
+    console.log(`your server is running on port ${port}`);
+  });
+}
+
+
+start()
+  .then(() => {
+    console.log('Application started');
+  })
+  .catch((error) => {
+    console.log('Application failed to start', error);
+  });
