217fff9cc3d4db580c8d8e20c0aac58f24ccfbb4
Nikita Karpuk <bitxon.dev@gmail.com>
2023-10-13 19:05:08 +0200

    7 Pods

--- hazelcast-client-server/src/main/java/bitxon/hz/HzConfiguration.java
+++ hazelcast-client-server/src/main/java/bitxon/hz/HzConfiguration.java
@@ -3,15 +3,23 @@ package bitxon.hz;
 
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.HazelcastInstance;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Slf4j
 @Configuration
 public class HzConfiguration {
 
     @Bean // Spring automatically detects method shutdown() as destroyMethod
     public HazelcastInstance hazelcastInstance() {
         // By default, it will try to load configuration from hazelcast-client.yaml or hazelcast-client.xml
-        return HazelcastClient.newHazelcastClient();
+        var start = LocalDateTime.now();
+        var instance = HazelcastClient.newHazelcastClient();
+        log.info("Hazelcast started @{} sec", Duration.between(start, LocalDateTime.now()).getSeconds());
+        return instance;
     }
 }
--- hazelcast-embedded/src/main/java/bitxon/hz/HzConfiguration.java
+++ hazelcast-embedded/src/main/java/bitxon/hz/HzConfiguration.java
@@ -3,15 +3,24 @@ package bitxon.hz;
 
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Period;
+
+@Slf4j
 @Configuration
 public class HzConfiguration {
 
     @Bean // Spring automatically detects method shutdown() as destroyMethod
     public HazelcastInstance hazelcastInstance() {
         // By default, it will try to load configuration from hazelcast.yaml or hazelcast.xml
-        return Hazelcast.newHazelcastInstance();
+        var start = LocalDateTime.now();
+        var instance = Hazelcast.newHazelcastInstance();
+        log.info("Hazelcast started @{} sec", Duration.between(start, LocalDateTime.now()).getSeconds());
+        return instance;
     }
 }
